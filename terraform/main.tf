d5f823f54a4768a710aa3823865320fee4504fcf
abdulwahab4d-max <abdul.wahab.4d@gmail.com>
2026-01-04 12:13:12 +0500

    allowing to listen the port 3000

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,6 +161,14 @@ resource "aws_security_group" "ecs_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  description     = "ALB to link-service"
+  from_port       = 3000
+  to_port         = 3000
+  protocol        = "tcp"
+  security_groups = [aws_security_group.alb_sg.id]
+}
+
 
   # Container-to-container
   ingress {
@@ -285,7 +293,6 @@ resource "aws_lb_listener_rule" "link_rule" {
     }
   }
 }
-
 
 # ----------------------------
 # ECS Cluster
