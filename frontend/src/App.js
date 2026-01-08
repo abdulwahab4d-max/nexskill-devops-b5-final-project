73a6685d7574e0cd21ccf9107111f60d825eb3ea
abdulwahab4d-max <abdul.wahab.4d@gmail.com>
2026-01-08 16:17:07 +0500

    analytics & link-service config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const LINK_SERVICE_URL = "/api";
-const ANALYTICS_SERVICE_URL = process.env.REACT_APP_ANALYTICS_SERVICE_URL || 'http://analytics-service:4000';
+const ANALYTICS_SERVICE_URL = "/api";
 
 
 function App() {
